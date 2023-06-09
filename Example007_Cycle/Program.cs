4c43c913410fe01f95cab1bb86b203b77ea0d1c3
MarinLIS <malinalu@mail.ru>
2023-06-09 12:33:59 +0400

    границы не попадают

--- Example007_Cycle/Program.cs
+++ Example007_Cycle/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.Clear();
-Console.SetCursorPosition (10, 4);
+Console.SetCursorPosition (80, 4);
 Console.WriteLine ("+");
 
+
 int xa = 1, ya = 1,
-    xb = 1, yb = 20,
-    xc = 20, yc = 20;
+    xb = 1, yb = 30,
+    xc = 30, yc = 80;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine ("+");
@@ -17,15 +18,15 @@ Console.WriteLine("+");
 
 int x = xa, y = xb;
 
-int count = 1;
+int count = 0;
 
-while (count < 10000);
+while (count < 10)
 {
     int what = new Random().Next(0, 3); // [0;3] 0 1 2
     if (what == 0)
     {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
+        x = (x+xa) / 2;
+        y = (y+ya) / 2;
     }
     if (what == 1)
     {
@@ -34,8 +35,8 @@ while (count < 10000);
     }
     if (what == 2)
     {
-        x = (x+ xc)/2;
-        y = (y+ yc)/2;
+        x = (x+xc)/2;
+        y = (y+yc)/2;
     }
     Console.SetCursorPosition (x, y);
     Console.WriteLine("+");
