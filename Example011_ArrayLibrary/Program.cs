c0491c88ea74d8df9dd30edf97a4b1200be61f65
MarinLIS <malinalu@mail.ru>
2023-06-13 18:00:11 +0400

    написали программу ввода случайных чисел и вывода их на экран и поиск нужного нам числа

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿// int [] array = new int[10]; // создай новый массив, где будет 10 жлементов
+
+void FillArray (int[] collection);
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+       collection [index] = new Random ().Next (1, 10);
+        index++;
+    }
+}
+
+void PrintArray (int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine (col [position]);
+        position ++;
+    }
+}
+
+int Indexof (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // если элемента нет, то будет выводится -1
+
+    while (index < count)
+    {
+        if(collection[index]== find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int [] array = new int [10]; // определили массив
+
+
+FillArray (array); // заполнили новыми элементами
+array[4] = 4;
+array[6] = 4;
+PrintArray (array); // напечатали случайными элементами
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
+
